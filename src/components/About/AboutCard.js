a908c8a87e837076897a4680a061f09414b89a19
RishabhSingh <60999728+ryshabh20@users.noreply.github.com>
2023-11-20 09:48:29 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,12 +7,11 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
+          <p style={{ textAlign: "center" }}>
             Hi Everyone, I am <span className="purple">Rishabh Singh </span>
             from <span className="purple"> Delhi, India.</span>
-            <br /> I am a full stack web developer with a passion for turning
-            ideas into pixel-perfect reality. From front-end magic to back-end
-            wizardry
+            <br />I'm your go-to Full Stack Developer, ready to bring your dream product to life in the virtual world. From crafting sleek websites for small and medium-sized businesses to empowering you by building your dream tech product, l've got the skills and expertise to make it happen.
+With a mastery of JavaScript, React.js, Node.js, express.js, CSS and Next.js, I can help ensure your online presence stands out from the crowd. I am skilled in creating user-friendly interfaces, building RESTful APIs, and seamlessly integrating external services. Besides, I am a technical lead/project manager, who prefers to meticulously oversee every aspect of a project, from conception to completion, leaving no room for mediocrity.
             <br />
           </p>
     
