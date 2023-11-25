cd9258857c72c18187ff3b2012d9994cdd73da90
Rishabh <rishabdahiya98@gmail.com>
2023-11-25 19:10:24 +0530

    added a resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,56 +1,57 @@
-// import React, { useState, useEffect } from "react";
-// import { Container, Row } from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
-// import Particle from "../Particle";
-// import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-// import { AiOutlineDownload } from "react-icons/ai";
-// import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/Resume.pdf";
 
-// function ResumeNew() {
-//   const [width, setWidth] = useState(1200);
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-//   useEffect(() => {
-//     setWidth(window.innerWidth);
-//   }, []);
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
 
-//   return (
-//     <div>
-//       <Container fluid className="resume-section">
-//         <Particle />
-//         <Row style={{ justifyContent: "center", position: "relative" }}>
-//           <Button
-//             variant="primary"
-//             href={pdf}
-//             target="_blank"
-//             style={{ maxWidth: "250px" }}
-//           >
-//             <AiOutlineDownload />
-//             &nbsp;Download CV
-//           </Button>
-//         </Row>
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
-//         <Row className="resume">
-//           <Document file={pdf} className="d-flex justify-content-center">
-//             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-//           </Document>
-//         </Row>
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
 
-//         <Row style={{ justifyContent: "center", position: "relative" }}>
-//           <Button
-//             variant="primary"
-//             href={pdf}
-//             target="_blank"
-//             style={{ maxWidth: "250px" }}
-//           >
-//             <AiOutlineDownload />
-//             &nbsp;Download CV
-//           </Button>
-//         </Row>
-//       </Container>
-//     </div>
-//   );
-// }
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
 
-// export default ResumeNew;
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
